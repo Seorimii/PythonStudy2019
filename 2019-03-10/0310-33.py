a8a6c0b964dd00d5bea47ec0d960faa732852c35
Lee_Seorim <tothegalaxy18@gmai.com>
2019-03-17 01:29:15 +0900

    파이썬 스터디 4회차

---
+++ 2019-03-10/0310-33.py
@@ -0,0 +1,8 @@
+strdata1='나는 파이썬 프로그래머다'
+strdata2="You are a programmmer"
+strdata3="""I love
+    python. You love
+python too!    
+"""
+strdata4="My sons name is John"
+strdata5='문자열"abc"의 길이는 3입니다.'
