5997cbda9e82469c33f717ce6c928de6f2c2acad
Lee_Seorim <tothegalaxy18@gmai.com>
2019-03-17 11:55:35 +0900

    파이썬 스터디 5회차

---
+++ mar_17/0317-43-01.py
@@ -0,0 +1,4 @@
+import mylib
+
+ret1=mylib.add_txt('대한민국','1등')
+ret2=mylib.reverse(1,2,3,)
---
+++ mar_17/0317-44.py
@@ -0,0 +1,7 @@
+import mar_17.mypackage.mylib
+
+ret1= mar_17.mypackage.mylib.add_txt('대한민국','1등')
+ret2= mar_17.mypackage.mylib.reverse(1,2,3)
+
+print(ret1)
+print(ret2)
